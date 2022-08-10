d51a75e3fa874428ae3758b308c93d1cdc87ae1a
Anusree1234sreenivasan <110896832+Anusree1234sreenivasan@users.noreply.github.com>
2022-08-10 13:51:42 +0530

    fifth commit

---
+++ newone.py
@@ -0,0 +1,10 @@
+b="Hi Anu"
+print(b.upper())
+print(b.lower())
+print(b.capitalize())
+var="  Hi Anu  "
+print(var.strip())
+print(var.lstrip())
+print(var.rstrip())
+var="Hi Anu"
+print(var.replace("Anu","Achu"))
